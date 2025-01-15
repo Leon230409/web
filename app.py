2e31ef3d25c97f43888e9f31b2575aba39702ded
Leon230409 <laimoprya@yandex.ru>
2025-01-15 17:27:41 -0300

    gta

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from jinja2 import FileSystemLoader
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 @app.route('/films')
 def films():
